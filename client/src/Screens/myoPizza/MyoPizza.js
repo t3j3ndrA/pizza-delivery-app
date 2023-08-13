d941114df2964e4d19e79096f7db8b77403c5cc3
tejendra.dhanani <tejendra.dhanani@pirimidtech.com>
2023-08-13 14:46:08 +0530

    Initial commit

---
+++ client/src/Screens/myoPizza/MyoPizza.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyoPizzaDisplay from '../../Components/MyoPizzaDisplay/MyoPizzaDisplay';
+import Navbar from '../../Components/Navbar/Navbar';
+export default function MyoPizza() {
+	return (
+		<div>
+			<Navbar />
+			<MyoPizzaDisplay />
+		</div>
+	);
+}
